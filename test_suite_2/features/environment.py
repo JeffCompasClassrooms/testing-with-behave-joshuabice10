7eee6ca209f511f8443395eea2f690e2464d51ae
Josh <joshuabice10@gmail.com>
2025-11-15 20:45:26 -0700

    Changed environment.py in both test suites

--- test_suite_2/features/environment.py
+++ test_suite_2/features/environment.py
@@ -1,7 +1,16 @@
-import behave_webdriver
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 def before_all(context):
-    context.behave_driver = behave_webdriver.Chrome()
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+
+    service = Service(ChromeDriverManager().install())
+    context.behave_driver = webdriver.Chrome(service=service, options=options)
 
 def after_all(context):
-    context.behave_driver.quit()
+    if hasattr(context, "behave_driver"):
+        context.behave_driver.quit()
